c31c530c32507f2009614d9a8a495954aa11b5b2
hjh <haojunhao99@163.com>
2023-09-01 14:46:52 +0800

    init

---
+++ assets/info.17231e29.js
@@ -0,0 +1,1 @@
+import{q as e}from"./index-4a7ae2f2.js";const t=e("person",{state:()=>({url:"/static/head.jpg",name:"马冬梅",sex:"男",number:"202120048",depart:"工学部"}),actions:{setUrl(e){this.url=e},setName(e){this.name=e},setSex(e){this.sex=e},setNumber(e){this.number=e},setDepart(e){this.depart=e}}});export{t as u};
